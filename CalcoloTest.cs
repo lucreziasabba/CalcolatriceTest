81a27ad3c2c283b9d8e557016bfac0e2d4f53ed3
lucreziasabba <55913152+lucreziasabba@users.noreply.github.com>
2019-11-22 15:17:56 +0100

    Add files via upload

---
+++ CalcoloTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcolatriceLibreria;
+
+namespace Calcolo_Tests
+{
+    [TestClass]
+    public class CalcTest
+    {
+        public void SommaPositiviTest()
+        { 
+            int n1 = 9;
+            int n2 = 4;
+            int somma_aspettata = 13;
+            int somma= Calcolo.Somma(n1, n2);
+            Assert.AreEqual(somma_aspettata, somma);
+        }
+        public void DivisionePositiviTest()
+        {
+            int n1 = 8;
+            int n2 = 4;
+            int divisione_aspettata = 2;
+            int divisione = Calcolo.Divisione(n1, n2);
+            Assert.AreEqual(divisione_aspettata, divisione);
+        }
+    }
+}
